388d865d3e8dff2b202191b9f6bf4c3fd4e708ba
jaydev singh <109211337+jaydevsinghwork24@users.noreply.github.com>
2022-08-14 17:03:47 +0530

    comment added, print statement modified

--- small.py
+++ small.py
@@ -1,7 +1,8 @@
 import numpy as np
 arr = np.array([9,2,7,4,6,8])
 small = arr[0]
+# loop to find smallest number
 for i in range(len(arr)):
     if small > arr[i]:
         small=arr[i]
-print('smallest is ', small)
+print('smallest no is ', small)
